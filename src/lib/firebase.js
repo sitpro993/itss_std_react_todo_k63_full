5fc01578359e74343795c0be1fa6b1f9a5784db7
Nguyễn Văn Hồng <70307363+sitpro993@users.noreply.github.com>
2022-06-23 15:44:02 +0700

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcqGPlP5U6RADPT1yB3uBRHaIIGhw3CVc",
+  authDomain: "fir-sample-71136.firebaseapp.com",
+  projectId: "fir-sample-71136",
+  storageBucket: "fir-sample-71136.appspot.com",
+  messagingSenderId: "658324240739",
+  appId: "1:658324240739:web:fde6816c7c24b762147b56",
+};
+
+firebase.initializeApp(firebaseConfig);
